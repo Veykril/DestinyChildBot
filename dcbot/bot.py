0ff69b1814f5e32378cd8fa1a3a7446de1539fda
Lukas Wirth <lukastwg@yahoo.de>
2016-11-26 16:11:30 +0100

    changed the way commands interpret

--- dcbot/bot.py
+++ dcbot/bot.py
@@ -6,7 +6,7 @@ from dcbot.constants import *
 from functools import wraps
 
 import discord
-
+import inspect
 import traceback
 
 
@@ -23,7 +23,7 @@ class DestinyChildBot(discord.Client):
     def superuser(func):
         @wraps(func)
         async def wrapper(self, **kwargs):
-            if self.perm_mngr.is_superuser(kwargs['author'].id):
+            if self.perm_mngr.is_superuser(kwargs['message'].author.id):
                 return await func(self, **kwargs)
             return  # skip if not in superuser list
         return wrapper
@@ -44,30 +44,6 @@ class DestinyChildBot(discord.Client):
 
         content = message.content.strip()  # type: str
 
-        if content.startswith(self.config.command_trigger):  # command parser
-            command, *args = content[len(self.config.command_trigger):].split()
-            for i in range(len(args)):
-                try:
-                    args[i] = int(args[i])
-                except ValueError:
-                    pass
-
-            try:  # TO-DO: Make the command system nicer
-                cmd_func = getattr(self, 'c_{}'.format(command))
-
-                func_kwargs = dict()
-                func_kwargs['message'] = message
-                func_kwargs['author'] = message.author
-                func_kwargs['args'] = args
-
-                print("Calling function:", cmd_func)
-                await cmd_func(**func_kwargs)
-            except AttributeError:
-                pass
-            except Exception as e:
-                await self.send_message(message.channel, "```{}```".format(traceback.format_exc()))
-            return
-
         left_index = 0
         activators = []
         while True:
@@ -80,12 +56,58 @@ class DestinyChildBot(discord.Client):
             activators.append(content[left_index+1: right_index])
             left_index = right_index+1
 
+        children = []
         for id in activators:
             try:  # check if it's the id as keyword
                 identifier = int(id)
             except ValueError:
                 identifier = id
             child = self.children_mngr.get_child_by_identifier(identifier)
+            if not child:
+                await self.send_message(message.channel, "Didn't recognize child name/id: {}".format(id))
+            children.append(child)
+
+        if content.startswith(self.config.command_trigger):  # command parser
+            command, *args = content[len(self.config.command_trigger):].split()
+            i = 0
+            l = len(args)
+            while i < l:
+                try:
+                    args[i] = int(args[i])
+                    i += 1
+                except ValueError:
+                    if args[i][0] == self.config.activator_left and args[i][-1] == self.config.activator_right:
+                        del args[i]
+                        l -= 1
+                    else:
+                        i += 1
+
+            try:
+                cmd_func = getattr(self, 'c_{}'.format(command), None)
+                if not cmd_func:
+                    return
+
+                parameters = inspect.signature(cmd_func).parameters.copy()
+
+                func_kwargs = dict()
+                if parameters.pop('message', None):
+                    func_kwargs['message'] = message
+                if parameters.pop('channel', None):
+                    func_kwargs['channel'] = message.channel
+                if parameters.pop('author', None):
+                    func_kwargs['author'] = message.author
+                if parameters.pop('children', None):
+                    func_kwargs['children'] = children
+                if parameters.pop('args', None):
+                    func_kwargs['args'] = args
+
+                print("Calling function:", cmd_func)
+                await cmd_func(**func_kwargs)
+            except Exception as e:
+                await self.send_message(message.channel, "```{}```".format(traceback.format_exc()))
+            return
+
+        for child in children:
             if child:
                 emb = discord.Embed(type='rich', colour=DestinyChildBot.ele_color[child[JSON_ATTRIBUTE_ID]])
                 emb.set_author(name="{} - {}[{}⭐]".format(child[JSON_NAME], child[JSON_EN_NAME], child[JSON_RARITY]))
@@ -95,25 +117,24 @@ class DestinyChildBot(discord.Client):
                 await self.send_message(message.channel, embed=emb)
 
     @superuser
-    async def c_debug(self, **kwargs):
+    async def c_debug(self, message):
         self.config.debug = not self.config.debug
         print("Debug toggled to", self.config.debug)
 
     @superuser
-    async def c_add_superuser(self, **kwargs):
-        self.perm_mngr.add_superuser(kwargs['args'][0])
+    async def c_add_superuser(self, message, args):
+        self.perm_mngr.add_superuser(args[0])
 
     @superuser
-    async def c_nickname(self, **kwargs):
-        args = kwargs['args']
-        if len(args) == 2:
-            self.children_mngr.add_nickname(args[0], args[1])
+    async def c_nickname(self, message, children, args):
+        if len(args) == 1 and len(children) == 1:
+            self.children_mngr.add_nickname(children[0][JSON_EN_NAME], args[0])
         else:
-            await self.send_message(kwargs['channel'], "Couldn't add nickname to child")
+            await self.send_message(message.channel, "Couldn't add nickname to child")
 
-    async def c_info(self, **kwargs):
-        c = self.children_mngr.get_child_by_identifier(kwargs['args'][0])
-        if c is not None:
+    async def c_info(self, message, children):
+        if children:
+            c = children[0]
             emb = discord.Embed(type='rich', colour=DestinyChildBot.ele_color[c[JSON_ATTRIBUTE_ID]])
             emb.set_author(name="{} - {}[{}⭐]".format(c[JSON_NAME], c[JSON_EN_NAME], c[JSON_RARITY]))
             emb.description = "Role: {} | Element: {}".format(Role(c[JSON_ROLE_ID]).name.capitalize(),
@@ -124,13 +145,11 @@ class DestinyChildBot(discord.Client):
             emb.add_field(name='Defense', value=c[JSON_STAT_DEF], inline=False)
             emb.add_field(name='Critical', value=c[JSON_STAT_CRIT], inline=False)
             emb.set_thumbnail(url="{}{}_i.png".format(INVEN_IMAGE_URL, c[JSON_INVEN_ID]))
-            await self.send_message(kwargs['message'].channel, embed=emb)
-        else:
-            await self.send_message(kwargs['message'].channel, "Didn't recognize child name: {}".format(kwargs['args'][0]))
+            await self.send_message(message.channel, embed=emb)
 
-    async def c_skills(self, **kwargs):
-        c = self.children_mngr.get_child_by_identifier(kwargs['args'][0])
-        if c is not None:
+    async def c_skills(self, message, children):
+        if children:
+            c = children[0]
             emb = discord.Embed(type='rich', colour=DestinyChildBot.ele_color[c[JSON_ATTRIBUTE_ID]])
             emb.set_author(name="{} - {}'s skills".format(c[JSON_NAME], c[JSON_EN_NAME]))
             emb.add_field(name="Basic Attack", value=c[JSON_SKILL1_DESC_EN] or c[JSON_SKILL1_DESC], inline=False)
@@ -139,4 +158,4 @@ class DestinyChildBot(discord.Client):
             emb.add_field(name="Drive", value=c[JSON_SKILL4_DESC_EN] or c[JSON_SKILL4_DESC], inline=False)
             emb.add_field(name="Leader", value=c[JSON_SKILL5_DESC_EN] or c[JSON_SKILL5_DESC], inline=False)
             emb.set_thumbnail(url="{}{}_i.png".format(INVEN_IMAGE_URL, c[JSON_INVEN_ID]))
-            await self.send_message(kwargs['message'].channel, embed=emb)
+            await self.send_message(message.channel, embed=emb)
